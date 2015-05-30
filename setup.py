51071664db97e80f2a9d146d7629f928942dfab4
Alexander Zelenyak <zzz.sochi@gmail.com>
2015-05-30 22:03:32 +0300

    Update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name='resolver_deco',
-      version='1.0.1',
+      version='1.0.2',
       description='Decorator for resolve function arguments',
       # long_description=README,
       classifiers=[
